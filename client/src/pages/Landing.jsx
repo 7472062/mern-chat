4680286d04e4c0eefec4df406dabc9a6f8d67ea6
7472062 <7472062@gmail.com>
2025-05-26 21:29:17 +0900

    기본 페이지 구성

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+function Landing() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const accessToken = sessionStorage.getItem('accessToken');
+        if (accessToken) {
+            navigate('/chat');
+        }
+    }, [navigate]);
+
+    return (
+        <div className='landing-container'>
+            <h1>Concord</h1>
+            <div className='button-group'>
+                <Link to='/login'>
+                    로그인
+                </Link>
+                <Link to='/register'>
+                    회원가입
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;

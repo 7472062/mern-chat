5a7624712f059d193dc2ca3a122df591a07b64f0
7472062 <7472062@gmail.com>
2025-05-27 13:03:15 +0900

    랜딩페이지 구성

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+
+import './Landing.css';
 
 function Landing() {
     const navigate = useNavigate();
